d10c51319abf036e95513aed6821b902a7253300
Martin Kleinschrodt <martin@maklesoft.com>
2016-09-05 10:25:26 +0200

    Optimise javascript loading and tracking

--- static/js/home.js
+++ static/js/home.js
@@ -1,57 +1,58 @@
-window.onload = function() {
-    mixpanel.track_links(".source-link", "open source");
-    mixpanel.track_links(".download-link", "open chrome");
-    mixpanel.track_links(".maklesoft-link", "open maklesoft website");
-    mixpanel.track_forms("#mc-embedded-subscribe-form", "subscribe to newsletter");
+(function() {
+    function getPlatform() {
+        var isIOS = function() {
+            return /ipad|iphone|ipod/i.test(navigator.platform);
+        };
 
-    var iDevices = ["iPad", "iPhone", "iPod"];
-    function isIOS() {
-        return iDevices.reduce(function(match, dev) {
-            return match || navigator.platform.indexOf(dev) !== -1;
-        }, false);
-    };
+        var isAndroid = function() {
+            return /android/i.test(navigator.userAgent);
+        };
 
-    function isAndroid() {
-        return navigator.userAgent.toLowerCase().indexOf("android") !== -1;
+        var platform = "other";
+        if (isIOS()) {
+            platform = "ios";
+        } else if (isAndroid()) {
+            platform = "android";
+        }
+        return platform;
     }
 
-    var downloadUrls = {
-        "ios": "https://itunes.apple.com/app/id871710139",
-        "android": "https://play.google.com/store/apps/details?id=com.maklesoft.padlock",
-        "other": "https://chrome.google.com/webstore/detail/padlock/npkoefjfcjbknoeadfkbcdpbapaamcif"
-    };
+    function setupDownloadButton() {
+        var downloadUrls = {
+            "ios": "https://itunes.apple.com/app/id871710139",
+            "android": "https://play.google.com/store/apps/details?id=com.maklesoft.padlock",
+            "chrome": "https://chrome.google.com/webstore/detail/padlock/npkoefjfcjbknoeadfkbcdpbapaamcif"
+        };
 
-    var buttonCaptions = {
-        "ios": "Available on the App Store",
-        "android": "Available on Google Play",
-        "other": "Add to Chrome"
-    };
+        var buttonCaptions = {
+            "ios": "Available on the App Store",
+            "android": "Available on Google Play",
+            "chrome": "Add to Chrome"
+        };
 
-    var platform = "other";
-    if (isIOS()) {
-        platform = "ios";
-    } else if (isAndroid()) {
-        platform = "android";
+        var button = document.getElementById("downloadButton");
+        var platform = getPlatform();
+        platform = platform == "other" ? "chrome" : platform;
+
+        button.innerHTML = buttonCaptions[platform];
+        button.href = downloadUrls[platform];
+        button.setAttribute("data-platform", platform);
+
+        var previewEl = document.querySelector(".preview");
+        var previewButton = document.querySelector(".preview-button");
+        var previewCloseButton = document.querySelector(".preview .close-button");
+        var iframe = previewEl.querySelector("iframe");
+
+        previewButton.addEventListener("click", function() {
+            previewEl.style.display = "block";
+            iframe.src = "https://player.vimeo.com/video/139904442?autoplay=1";
+        });
+
+        previewCloseButton.addEventListener("click", function() {
+            previewEl.style.display = "";
+            iframe.src = "";
+        });
     }
 
-    var button = document.getElementById("downloadButton");
-
-    button.innerHTML = buttonCaptions[platform];
-    button.href = downloadUrls[platform];
-    document.body.classList.add(platform);
-
-    var previewEl = document.querySelector(".preview");
-    var previewButton = document.querySelector(".preview-button");
-    var previewCloseButton = document.querySelector(".preview .close-button");
-    var iframe = previewEl.querySelector("iframe");
-
-    previewButton.addEventListener("click", function() {
-        previewEl.style.display = "block";
-        iframe.src = "https://player.vimeo.com/video/139904442?autoplay=1";
-    });
-
-    previewCloseButton.addEventListener("click", function() {
-        previewEl.style.display = "";
-        iframe.src = "";
-    });
-}
+    setupDownloadButton();
+})();
